abb1db3c3de44d446e0593c1eb23535d04c691b0
Moritz Beller (Inventitech) <moritzbeller@gmx.de>
2019-03-07 17:16:25 +0100

    Add MacOS bundling

--- bundle_macos.sh
+++ bundle_macos.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+APP=bin/Release/netcoreapp2.2/osx-x64/publish/strans.app
+DMG_NAME=strans
+
 dotnet bundle -r osx-x64 -c Release
+hdiutil create -volname ${DMG_NAME} -srcfolder ${APP} -ov -format UDZO ${DMG_NAME}.dmg

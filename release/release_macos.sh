3deb301d441b5db227dbd43d89b3655530a3fe5c
Moritz Beller (Inventitech) <moritzbeller@gmx.de>
2019-03-08 10:30:20 +0100

    Reorganize repository layout

--- release/release_macos.sh
+++ release/release_macos.sh
@@ -4,4 +4,4 @@ APP=bin/Release/netcoreapp2.2/osx-x64/publish/strans.app
 DMG_NAME=strans
 
 dotnet bundle -r osx-x64 -c Release
-hdiutil create -volname ${DMG_NAME} -srcfolder ${APP} -ov -format UDZO ${DMG_NAME}-macos.dmg
+hdiutil create -volname ${DMG_NAME} -srcfolder ${APP} -ov -format UDZO release/${DMG_NAME}-macos.dmg

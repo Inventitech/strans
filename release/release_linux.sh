0433afa52627d65262d8d972b285b55a2bb87374
Moritz Beller (Inventitech) <moritzbeller@gmx.de>
2019-03-29 16:35:47 +0100

    Add check for existing App tool

--- release/release_linux.sh
+++ release/release_linux.sh
@@ -7,6 +7,10 @@ dotnet publish -c Release -r linux-x64
 cp bin/Release/netcoreapp2.2/linux-x64/publish/* release/AppDir/usr/bin/
 
 cd release
+if [ -e appimagetool-x86_64.AppImage ]
+then
+   rm appimagetool-x86_64.AppImage
+fi
 wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod +x appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage AppDir
